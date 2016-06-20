a55e45d54b8bced579923e917908d254362fb543
Shaguftarafi <12105024@gift.edu.pk>
2016-06-20 22:30:05 +0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace MyFYP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<string> growingList = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string path = "D:\\GUIS\\WpfApplication3\\wordfrequency.txt";
+
+
+
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Show();
+        }
+
+
+        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                textbox.Text = filename;
+
+                System.IO.StreamReader sr = new System.IO.StreamReader(dlg.FileName);
+
+                textbox.Text = System.IO.File.ReadAllText(filename);
+                sr.Close();
+
+            }
+        }
+        //Specl
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            string a = searchbox.Text;
+            SpellChecker shapexObject = new SpellChecker();
+            char[] codeForInput = shapexObject.shapex(a);
+            string LexiconWord;
+            List<string> list = new List<string>(); //List is a name of Collection to contain possible correct words
+            StreamReader file = new StreamReader(path);
+            while ((LexiconWord = file.ReadLine()) != null)
+            {
+                char[] LexiconWordCode = shapexObject.shapex(LexiconWord);
+                if (codeForInput[0] == LexiconWordCode[0] && codeForInput[1] == LexiconWordCode[1]
+                   && codeForInput[2] == LexiconWordCode[2] && codeForInput[3] == LexiconWordCode[3]
+                   && codeForInput[4] == LexiconWordCode[4])// && codeForInput[5] == LexiconWordCode[5]
+                {
+                    listbox1.Items.Add(LexiconWord);
+                    //listBox8.Items.Add(LexiconWord);
+                    //listBox7.Items.Add(LexiconWord);
+                    list.Add(LexiconWord);
+                }
+
+            }// end while-loop
+            string[] array1 = list.ToArray();
+            allCorrectWords(array1); // allCorrectWords() is a function to store and sort
+        }// end shapex button-function
+
+        // **************************** End Shapex *********************************//
+        // **************************** End Shapex *********************************//
+        // **************************** End Shapex *********************************//
+
+
+
+        // allCorrectWords() is a function to store and sort
+        public void allCorrectWords(string[] abc)
+        {
+            //listbox.Items.Clear();
+            // growingList has been declared as a public data object list
+            for (int zz = 0; zz < abc.Length; zz++)
+            {
+                if (growingList.Contains(abc[zz])) ;
+                else growingList.Add(abc[zz]);
+            }
+            string[] array = growingList.ToArray();     // array is a string array to contain all possible words
+            int a = array.Length;
+            string[,] _2D_array = new string[a, 2];    // _2D_array is a 2-D array to contain all possible words with their frequencies
+            for (int i = 0; i < a; i++)
+                _2D_array[i, 0] = array[i];             // copy all content of "array" to 2-D array at "column 0"
+            string[] line = new string[2];              // it will contain splitted line of each word and its frequency  
+            string LexiconWord;
+            List<string> words_n_frequency = new List<string>(); // it will have a "two times length" than frequency-word-List
+            StreamReader frequencyFile = new StreamReader(path);
+            while ((LexiconWord = frequencyFile.ReadLine()) != null)
+            {
+                line = LexiconWord.Split('\t');
+                words_n_frequency.Add(line[0]);
+                words_n_frequency.Add(line[1]);
+            }
+            int b = (words_n_frequency.Count) / 2;
+            string[,] _2D_words_n_frequency = new string[b, 2];
+
+            // words_n_frequency_array.Length = 10,000
+
+            string[] words_n_frequency_array = words_n_frequency.ToArray();
+            int z = 1;
+            for (int j = 0; j < words_n_frequency_array.Length; j += 2)
+            {
+                if (j == 0)
+                {
+                    _2D_words_n_frequency[j, 0] = words_n_frequency_array[j];
+                    _2D_words_n_frequency[j, 1] = words_n_frequency_array[j + 1];
+                }
+                else
+                {
+                    _2D_words_n_frequency[j - z, 0] = words_n_frequency_array[j];
+                    _2D_words_n_frequency[j - z, 1] = words_n_frequency_array[j + 1];
+                    z++;
+                }
+            } // now "_2D_words_n_frequency[,]" has words in "column-0" and their frequencies in "column-1"
+
+            for (int m = 0; m < a; m++) // a is length of "_2D_array"
+            {
+                for (int k = 0; k < b; k++) // a is half of length of "_2D_words_n_frequency[,]"
+                {
+                    if (_2D_array[m, 0] == _2D_words_n_frequency[k, 0])
+                    {
+                        _2D_array[m, 1] = _2D_words_n_frequency[k, 1];
+                        break;
+                    }
+                }
+            } // now "_2D_array[,]" has words in "column-0" and their frequencies in "column-1"
+            // now sorting starts
+
+            for (int i = 0; i < a - 1; i++)
+            {
+                for (int j = i + 1; j < a; j++)
+                    if (Convert.ToInt64(_2D_array[j, 1]) > Convert.ToInt64(_2D_array[i, 1]))
+                    {
+                        string[,] tmp = new string[1, 2];
+                        tmp[0, 0] = _2D_array[i, 0];
+                        tmp[0, 1] = _2D_array[i, 1];
+                        _2D_array[i, 0] = _2D_array[j, 0];
+                        _2D_array[i, 1] = _2D_array[j, 1];
+                        _2D_array[j, 0] = tmp[0, 0];
+                        _2D_array[j, 1] = tmp[0, 1];
+                    }
+            }
+            for (int i = 0; (i < 5 && i < a && _2D_array[i, 1] != null); i++)
+                listbox1.Items.Add(_2D_array[i, 0]);
+
+
+        }
+
+
+
+        private void listBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+
+
+            Process[] processlist = Process.GetProcesses(".");
+
+            foreach (Process p in processlist)
+            {
+                if (!String.IsNullOrEmpty(p.MainWindowTitle))
+                {
+                    TreeViewItem item = new TreeViewItem();
+                    // item.MouseDoubleClick += new MouseButtonEventHandler(item_MouseDoubleClick);
+                    item.Tag = p;
+                    item.Header = p.MainWindowTitle.ToString();
+                    listbox1.Items.Add(item);
+
+                    // Icon ico = Icon.ExtractAssociatedIcon(p.MainModule.FileName);
+                }
+
+            }
+        }
+
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        } //End of allCorrectWords()
+    }
+
+}
+
---
+++ SpellChecker.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFYP
+{
+    class SpellChecker
+    {
+   // ***************************Start (Function Multiple Minimum edit Distance)********************//
+        // ***************************Start (Function Multiple Minimum edit Distance) *******************//
+        // ***************************Start (Function Multiple Minimum edit Distance) *******************//
+        public int getMinEditDistance(string target, string source)
+        {
+            // insert # before target word
+            char[] tmp = target.ToArray();
+            List<char> forTarget = new List<char>(tmp);
+            forTarget.Insert(0, '#');
+            char[] targetArray = forTarget.ToArray();
+
+            // insert # before source word
+            char[] tmp1 = source.ToArray();
+            List<char> forSource = new List<char>(tmp1);
+            forSource.Insert(0, '#');
+            char[] sourceArray = forSource.ToArray();
+
+            int sub_const;
+            int n = targetArray.Length;           // n is target length & m is source length
+            int m = sourceArray.Length;
+            int[,] distance = new int[n, m];
+            distance[0, 0] = 0;
+            for (int i = 1; i < n; i++)
+                distance[i, 0] = distance[i - 1, 0] + 1;
+
+            for (int j = 1; j < m; j++)
+                distance[0, j] = distance[0, j - 1] + 1;
+
+            for (int i = 1; i < n; i++) // i in actual is denoting rows
+            {
+                for (int j = 1; j < m; j++) // j in actual denoting columns
+                {
+                    if (targetArray[i] == sourceArray[j]) // to understand order of target[i] and source[j] is very critical to understand
+                        sub_const = 0;
+                    else
+                        sub_const = 2;
+
+                    int a = Math.Min(distance[i - 1, j] + 1, distance[i - 1, j - 1] + sub_const);
+                    distance[i, j] = Math.Min(a, distance[i, j - 1] + 1);
+                }
+            }
+            return distance[n - 1, m - 1];// we created of this size
+        }
+
+        // ***************************End (Function Multiple Minimum edit Distance) *******************//
+        // ***************************End (Function Multiple Minimum edit Distance) *******************//
+        // ***************************End (Function Multiple Minimum edit Distance) *******************//
+
+
+        // ********************************Start (function Soundex)*******************************//
+        // ********************************Start (function Soundex)*******************************//
+        // ********************************Start (function Soundex)*******************************//
+
+        public char[] soundex(string forSoundex)
+        {
+            char[] for_soundex = forSoundex.ToCharArray();
+            List<char> tmp = new List<char>(for_soundex);
+
+            char[] code = new char[5] { '0', '0', '0', '0', '0' };
+            int m = 0;
+
+            for (int i = 0; i < tmp.Count; i++)
+            {
+                if (i == 0)
+                {
+                    if (tmp[i] == '\u062b' || tmp[i] == '\u0633' || tmp[i] == '\u0635' || tmp[i] == '\u0634')
+                        code[m] = '1';
+                    else if (tmp[i] == '\u062a' || tmp[i] == '\u0637' || tmp[i] == '\u0679' || tmp[i] == '\u0629')
+                        code[m] = '2';
+                    else if (tmp[i] == '\u0632' || tmp[i] == '\u0636' || tmp[i] == '\u0638' || tmp[i] == '\u0630')
+                        code[m] = '3';
+                    else if (tmp[i] == '\u062c' || tmp[i] == '\u0686')
+                        code[m] = '4';
+                    else if (tmp[i] == '\u0628' || tmp[i] == '\u067e' || tmp[i] == '\u0641' || tmp[i] == '\u0648')
+                        code[m] = '5';
+                    else if (tmp[i] == '\u062e' || tmp[i] == '\u06a9' || tmp[i] == '\u0642')
+                        code[m] = '6';
+                    else if (tmp[i] == '\u062f' || tmp[i] == '\u0688' || tmp[i] == '\u0631' || tmp[i] == '\u0691')
+                        code[m] = '7';
+                    else if (tmp[i] == '\u0646' || tmp[i] == '\u06ba' || tmp[i] == '\u0645')
+                        code[m] = '8';
+                    else if (tmp[i] == '\u06af' || tmp[i] == '\u063a')
+                        code[m] = '9';
+                    else if (tmp[i] == '\u0644')
+                        code[m] = 'a';
+                    else if (tmp[i] == '\u0698' || tmp[i] == '\u06cc')
+                        code[m] = 'b';
+                    else if (tmp[i] == '\u06c1' || tmp[i] == '\u062d' || tmp[i] == '\u06be')
+                        code[m] = 'c';
+                    else if (tmp[i] == '\u0627' || tmp[i] == '\u0622' || tmp[i] == '\u0639') // for alef, alef-madda, and aaen
+                        code[m] = 'd';
+                    else if (tmp[i] == '\u0648') // for vao
+                        code[m] = 'e';
+
+                }
+                if (i > 0 && m < 5) // it is very important to mention (m<5).
+                {
+                    if (tmp[i] == '\u062b' || tmp[i] == '\u0633' || tmp[i] == '\u0635' || tmp[i] == '\u0634')
+                    { if (code[m - 1] != '1') code[m] = '1'; }
+                    else if (tmp[i] == '\u062a' || tmp[i] == '\u0637' || tmp[i] == '\u0679' || tmp[i] == '\u0629')
+                    { if (code[m - 1] != '2')  code[m] = '2'; }
+                    else if (tmp[i] == '\u0632' || tmp[i] == '\u0636' || tmp[i] == '\u0638' || tmp[i] == '\u0630')
+                    { if (code[m - 1] != '3')   code[m] = '3'; }
+                    else if (tmp[i] == '\u062c' || tmp[i] == '\u0686')
+                    { if (code[m - 1] != '4')   code[m] = '4'; }
+                    else if (tmp[i] == '\u0628' || tmp[i] == '\u067e' || tmp[i] == '\u0641' || tmp[i] == '\u0648')
+                    { if (code[m - 1] != '5')    code[m] = '5'; }
+                    else if (tmp[i] == '\u062e' || tmp[i] == '\u06a9' || tmp[i] == '\u0642')
+                    { if (code[m - 1] != '6')   code[m] = '6'; }
+                    else if (tmp[i] == '\u062f' || tmp[i] == '\u0688' || tmp[i] == '\u0631' || tmp[i] == '\u0691')
+                    { if (code[m - 1] != '7')    code[m] = '7'; }
+                    else if (tmp[i] == '\u0646' || tmp[i] == '\u06ba' || tmp[i] == '\u0645')
+                    { if (code[m - 1] != '8')   code[m] = '8'; }
+                    else if (tmp[i] == '\u06af' || tmp[i] == '\u063a')
+                    { if (code[m - 1] != '9')    code[m] = '9'; }
+                    else if (tmp[i] == '\u0644')
+                    { if (code[m - 1] != 'a')   code[m] = 'a'; }
+                    else if (tmp[i] == '\u0698' || tmp[i] == '\u06cc')
+                    { if (code[m - 1] != 'b')   code[m] = 'b'; }
+                    else if (tmp[i] == '\u06c1' || tmp[i] == '\u062d' || tmp[i] == '\u06be')
+                    { if (code[m - 1] != 'c')    code[m] = 'c'; }
+                    else if (tmp[i] == '\u0627' || tmp[i] == '\u0622' || tmp[i] == '\u0639')
+                    { if (code[m - 1] != 'd')    code[m] = 'd'; }
+                    else if (tmp[i] == '\u0648')
+                    { if (code[m - 1] != 'e')    code[m] = 'e'; }
+                } if (m < 5 && code[m] != '0') m++;
+
+            } // end parent for-loop
+            return code;
+        } //end function soundex()
+
+        // ********************************End (function Soundex)*******************************//
+        // ********************************End (function Soundex)*******************************//
+
+
+        // ******************************Start (Function shapex) **************************//
+        // ******************************Start (Function shapex) **************************//
+
+        public char[] shapex(string forShapex)
+        {
+            char[] for_shapex = forShapex.ToCharArray();
+            List<char> tmp = new List<char>(for_shapex);
+            char[] code = new char[5] { '0', '0', '0', '0', '0' };
+            int m = 0;
+
+            for (int i = 0; i < tmp.Count; i++)
+            {
+                if (m < 5)
+                {
+                    if (tmp[i] == '\u0627' || tmp[i] == '\u0622' || tmp[i] == '\u0644')
+                        code[m] = '1';
+                    else if (tmp[i] == '\u0628' || tmp[i] == '\u067e' || tmp[i] == '\u06c1' || tmp[i] == '\u06cc')
+                        code[m] = '2';
+                    else if (tmp[i] == '\u062a' || tmp[i] == '\u0679' || tmp[i] == '\u062b' || tmp[i] == '\u0646')
+                        code[m] = '3';
+                    else if (tmp[i] == '\u062d' || tmp[i] == '\u062e' || tmp[i] == '\u062c' || tmp[i] == '\u0686')
+                        code[m] = '4';
+                    else if (tmp[i] == '\u062f' || tmp[i] == '\u0688' || tmp[i] == '\u0630' || tmp[i] == '\u0631'
+                        || tmp[i] == '\u0691' || tmp[i] == '\u0632' || tmp[i] == '\u0698' || tmp[i] == '\u0648')
+                        code[m] = '5';
+                    else if (tmp[i] == '\u0635' || tmp[i] == '\u0636')
+                        code[m] = '6';
+                    else if (tmp[i] == '\u0637' || tmp[i] == '\u0638')
+                        code[m] = '7';
+                    else if (tmp[i] == '\u0641' || tmp[i] == '\u0642')
+                        code[m] = '8';
+                    else if (tmp[i] == '\u06a9' || tmp[i] == '\u06af')
+                        code[m] = '9';
+                    else if (tmp[i] == '\u0633' || tmp[i] == '\u0634')
+                        code[m] = 'a';
+                    else if (tmp[i] == '\u0639' || tmp[i] == '\u063a')
+                        code[m] = 'b';
+                    else if (tmp[i] == '\u0645')
+                        code[m] = 'c';
+                }
+                if (m < 5 && code[m] != '0') m++;
+            } //end for-loop
+            return code;
+        } // end function shapex()
+
+        // ******************************End (Function shapex) **************************//
+        // ******************************End (Function shapex) **************************//
+        // ******************************End (Function shapex) **************************//
+    }
+}
